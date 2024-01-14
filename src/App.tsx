5463072f6682cd2053dea7ed4f3140273016a170
Sonny Nguyen <sonnynguyen163@gmail.com>
2024-01-13 16:42:08 -0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+//import Films from './collections/Films';
+import uuid from "react-uuid";
+
+// {
+//   "films": "https://swapi.dev/api/films/",
+//   "people": "https://swapi.dev/api/people/",
+//   "planets": "https://swapi.dev/api/planets/",
+//   "species": "https://swapi.dev/api/species/",
+//   "starships": "https://swapi.dev/api/starships/",
+//   "vehicles": "https://swapi.dev/api/vehicles/"
+// }
 
 function App() {
+  const [collections, setCollections] = useState<string[]>([]);
+  const id = uuid();
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/")
+      .then((res) => res.json())
+      .then((data) => {
+        if (Object.keys(data).length) {
+          setCollections(Object.keys(data) as string[]);
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/people/1/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mt-10 flex items-start justify-center space-x-10">
+        {collections &&
+          collections.map((col, idx) => (
+            <button key={`${id}-${idx}`} className="btn btn-blue">
+              {col.charAt(0).toUpperCase() + col.slice(1)}
+            </button>
+          ))}
+      </div>
     </div>
   );
 }
