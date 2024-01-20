bec52ba792ffbbde29d7acc431da75bcd4f9c5b4
aleckslu <alecks.lu@gmail.com>
2024-01-20 06:40:32 -0800

    updated and added types to types.ts

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,17 +1,20 @@
-import axios, { Canceler } from "axios";
+import axios, { AxiosResponse, CancelToken} from "axios";
+import { PageData, People } from './types';
 
-export default {
-  get: (url: string, ...params: any[]) =>{
-    // const url = params[0]
+
+type Options = {
+  cancelToken: CancelToken
+}
+
+export const cache = {
+  get: (url: string, options?: Options): PageData<People> | Promise<AxiosResponse> => {
     const storedData = localStorage.getItem(url)
     
     if (storedData) {
-    return new Promise((resolve) => {
-      resolve(JSON.parse(storedData));
-      });
+    return JSON.parse(storedData);
     } else {
       return (
-          axios.get(url, ...params)
+          axios.get(url, options)
             .then(res => {
               localStorage.setItem(url, JSON.stringify(res))
               return res
